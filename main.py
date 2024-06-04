a77c05dd8819f335909c71e6297ace3b38e4e89f
narunraman <32226556+narunraman@users.noreply.github.com>
2024-06-04 12:40:28 -0700

    updated link buttons

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     st.title("STEER: Systematic and Tuneable Evaluation of Economic Rationality")
     st.logo('images/steer_logo.png', icon_image='images/steer_small.png')
     st.image('images/taxonomy.jpg')
-    st.header('A reproducible framework for evaluating economic rationality in foundation models.')
+    # st.header('A reproducible framework for evaluating economic rationality in foundation models.')
     # with stylable_container(key='link_buttons', css_styles = '''
     #     .stLinkButton:first-child {
     #         margin-right: 20px;
@@ -18,32 +18,34 @@ def main():
     #         flex-direction: row;
     #     }
     # '''):
-    st.markdown("""
-    <style>
-        .stButton>button {
-            width: 10vw;
-            height: 50px;
-            font-size: 20vw;
-            margin-right: 2vw;
-        }
-    </style>
-    """, unsafe_allow_html=True)
+    # st.markdown("""
+    # <style>
+    #     baseLinkButton-secondary>button {
+    #         width: 10vw !important;
+    #         height: 50px !important;
+    #         font-size: 20vw;
+    #         margin-right: 2vw;
+    #     }
+    # </style>
+    # """, unsafe_allow_html=True)
     button_cols = st.columns([0.15, 0.15, 0.7])
-    if button_cols[0].button('Paper'):
-        st.markdown("""
-        <script>
-            window.location.href = "https://arxiv.org/abs/2402.09552";
-        </script>
-        """, unsafe_allow_html=True)
+    # if button_cols[0].button('Paper'):
+    #     st.markdown("""
+    #     <script>
+    #         window.location.href = "https://arxiv.org/abs/2402.09552";
+    #     </script>
+    #     """, unsafe_allow_html=True)
 
-    if button_cols[1].button('Github'):
-        st.markdown("""
-        <script>
-            window.location.href = "https://github.com/narunraman/STEER";
-        </script>
-        """, unsafe_allow_html=True)
-    # st.button('Paper')#, 'https://arxiv.org/abs/2402.09552')
-    # st.button('Github')#, 'https://github.com/narunraman/STEER')
+    # if button_cols[1].button('Github'):
+    #     st.markdown("""
+    #     <script>
+    #         window.location.href = "https://github.com/narunraman/STEER";
+    #     </script>
+    #     """, unsafe_allow_html=True)
+    with button_cols[0]:
+        st.link_button('Paper', 'https://arxiv.org/abs/2402.09552')
+    with button_cols[1]: 
+        st.link_button('Github', 'https://github.com/narunraman/STEER')
     st.divider()
     # how to cite:
     st.header('Cite us if you use our work!')
